b40bced687f3ce6ed2341c6fdd0950ad3984ea2b
Mark Andrew Davis <markcagatandavis@gmail.com>
2017-10-08 21:59:28 +1100

    CompletedProject
    Completed small project001.

--- JSP001_ReverseAString/src/PlayArea.java
+++ JSP001_ReverseAString/src/PlayArea.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 
 public class PlayArea extends JPanel
 {
-    private JPanel outputJPanel, inputJPanel;
+    private JPanel outputJPanel, inputJPanel, outputPanel;
     private JTextField playerInput;
+    private JTextArea outputArea;
     private Font mainFont = new Font("Times New Roman", Font.PLAIN, 18);
     private JButton enterButton;
 
@@ -22,6 +20,14 @@ public class PlayArea extends JPanel
         outputJPanel.setBackground(Color.LIGHT_GRAY);
         outputJPanel.setLayout(new GridBagLayout());
         add(outputJPanel);
+
+        outputArea = new JTextArea();
+        outputArea.setBounds(0,0,400,200);
+        outputArea.setFont(mainFont);
+        outputArea.setOpaque(false);
+        outputArea.setVisible(true);
+        outputArea.setLayout(new GridBagLayout());
+        outputJPanel.add(outputArea);
 
         //This is used for JButton and JTextField.
         inputJPanel = new JPanel();
@@ -42,14 +48,24 @@ public class PlayArea extends JPanel
         enterButton.setHorizontalAlignment(SwingConstants.CENTER);
         enterButton.setPreferredSize(new Dimension(100,100));
         inputJPanel.add(enterButton);
+        enterButton.addActionListener(e -> {
 
-        playerInput.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                //Need to add code here.
-            }
+            String getText = playerInput.getText();
+            StringBuilder setReverseString = new StringBuilder(getText);
+            setReverseString.reverse();
+            String getReverseString =  setReverseString.toString();
+            outputArea.setText(getReverseString);
+            playerInput.setText("");
+        });
+
+        playerInput.addActionListener(e -> {
+
+            String getText = playerInput.getText();
+            StringBuilder setReverseString = new StringBuilder(getText);
+            setReverseString.reverse();
+            String getReverseString =  setReverseString.toString();
+            outputArea.setText(getReverseString);
+            playerInput.setText("");
         });
     }
 }
