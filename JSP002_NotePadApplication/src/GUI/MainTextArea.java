40f4beefd3838518ee64ef8eef77ce4d43da9421
markcagatandavis <markcagatandavis@gmail.com>
2017-10-24 22:11:50 +1100

    CleanUpCode
    CleanUpCode

--- JSP002_NotePadApplication/src/GUI/MainTextArea.java
+++ JSP002_NotePadApplication/src/GUI/MainTextArea.java
@@ -9,17 +9,17 @@ public class MainTextArea extends JPanel
     private JTextArea textArea;
     private JPanel textPanel;
 
-    private Font defaultFont = new Font("Times New Roman", Font.PLAIN, 12);
+    private Font defaultFont = new Font("Times New Roman", Font.PLAIN, 16);
 
     public MainTextArea()
     {
+
+        //Create Panel for text area to sit on
         textPanel = new JPanel();
         textPanel.setLayout(new BorderLayout());
         add(textPanel);
 
-        JScrollPane scrollPane = new JScrollPane();
-        add(scrollPane);
-
+        //Create main Typing area
         textArea = new JTextArea();
         textArea.setFont(defaultFont);
         textArea.setBackground(Color.white);
@@ -27,8 +27,11 @@ public class MainTextArea extends JPanel
         textArea.setLineWrap(true);
         textArea.setVisible(true);
 
+        //Add scroll pane
         JScrollPane scroll = new JScrollPane(textArea);
-        scroll.setPreferredSize(new Dimension(1280,1080));
+        scroll.setPreferredSize(new Dimension(1280,960));
+        scroll.setVisible(true);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         textPanel.add(scroll);
     }
 }
